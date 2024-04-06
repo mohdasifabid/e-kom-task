a7ef975365d7678df976ef81db65e14179498c63
Mohd Asif <masifabid@outlook.com>
2024-04-07 02:17:31 +0530

    Fix typo

--- app/ui/EmailVerification.tsx
+++ app/ui/EmailVerification.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "./Button";
-import { maskEmail, validateEmail, validatePassword } from "../lib/utils";
+import { maskEmail} from "../lib/utils";
 import { useEffect, useState } from "react";
 import OtpInput from "./OtpInput";
 
@@ -14,7 +14,7 @@ export const EmailVerification = (props: any) => {
   return (
     <div className="flex flex-col items-center border-2 border-gray-400 rounded-xl pl-12 pr-12 pb-4 w-576 h-576">
       <p className="font-inter text-3xl pt-16 font-semibold leading-38.73 text-left">
-        Verfify your Email
+        Verify your Email
       </p>
       <div className="font-inter text-base font-normal leading-4.84 text-center pt-6">
         <p>This is a div styled with Inter font.</p>
