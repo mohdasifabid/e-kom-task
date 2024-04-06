8af14a1193b7744c09a4828bfb2cf3c2a95379ae
Mohd Asif <masifabid@outlook.com>
2024-04-06 20:20:29 +0530

    Add email-verification route

---
+++ app/email-verification/page.tsx
@@ -0,0 +1,10 @@
+import { EmailVerification } from "../ui/EmailVerification";
+import { Layout } from "../ui/Layout";
+
+export default function EmailVerificatonPage() {
+  return (
+    <Layout>
+      <EmailVerification />
+    </Layout>
+  );
+}
