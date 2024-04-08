691cc4715c1e6bc0a36d280d25f5ee45cd859948
Mohd Asif <masifabid@outlook.com>
2024-04-08 23:25:21 +0530

    Change mail config secure to true

--- helpers/sendOtp.ts
+++ helpers/sendOtp.ts
@@ -13,7 +13,7 @@ export function sendOTP(email, otp) {
     const mailConfigOptions = {
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
-        secure: false,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
